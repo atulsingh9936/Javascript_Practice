d99b4ce663003b0ceda28adcbb9b204e64d23e49
Atul Singh <atul.singh2189@gmail.com>
2026-01-03 18:28:51 +0530

    functions

---
+++ Array.js
@@ -0,0 +1,81 @@
+// declaration of array
+let myArray=[];   // an empty array
+//let numbers=[1,2,3,4,5]; // an array with numbers
+//let fruits=["apple","banana","orange"]; // an array with strings
+
+
+let lang =["java","python","JS"];
+// 1. push
+lang.push("Ruby","HTML");
+console.log(lang);
+
+// 2. pop :
+let numbers =[1,2,3,4,5];
+numbers.pop();
+console.log(numbers);
+
+//3. shift:
+
+let fruits=["apple","banana","orange"]
+let firstfruit = fruits.shift();
+console.log(firstfruit);
+console.log(fruits);
+
+
+// 4. unshift 
+
+let colors = ['red','green','black','white'];
+console.log(colors.length);
+colors.unshift("blue","pink");
+console.log(colors);
+console.log(colors.length);
+
+// 5. splice
+
+let animals = ['dog','cat','bird','fish'];
+animals.splice(1,1,'bear');
+console.log(animals);
+
+// 6. slice
+let pop=[1,2,3,4,5];
+let newpop = pop.slice(1,4);
+console.log(newpop);
+
+// 7. concat
+
+let fr = ["apple","banana","orange"];
+let num =[1,2,3,4];
+let mixedArray= fr.concat(num);
+console.log(mixedArray);
+
+
+//8. indexof
+
+let color =["red","green","blue","red"];
+let indexRed = color.indexOf("red");
+console.log(indexRed);
+let naveenIndex = color.indexOf("naveen");
+console.log(naveenIndex);
+
+// 2nd red index:
+let redSecondIndex=color.indexOf("red",color.indexOf("red")+1);
+console.log(redSecondIndex);
+
+// 9. includes
+
+let test =["admin","customer","seller","vendor"];
+let flag = test.includes("seller","vendor");
+console.log(flag);
+
+// 10 for each
+
+let n = [1,2,3,4,5,6,7];
+n.forEach((e) => {
+  console.log(e);
+})
+
+
+
+
+
+
